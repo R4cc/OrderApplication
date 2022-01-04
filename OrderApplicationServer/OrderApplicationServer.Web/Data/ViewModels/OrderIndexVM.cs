173e4f121fb5ecd234577d94ce36b2eb522c5f81
Niklas L <niklas.lerchenmueller@protonmail.com>
2022-01-04 08:57:26 +0100

    Add order indexing

--- OrderApplicationServer/OrderApplicationServer.Web/Data/ViewModels/OrderIndexVM.cs
+++ OrderApplicationServer/OrderApplicationServer.Web/Data/ViewModels/OrderIndexVM.cs
@@ -7,5 +7,6 @@ namespace OrderApplicationServer.Web.Data.ViewModels
         public int OrderId { get; set; }
         public string UserId { get; set; }
         public DateTime OrderDate { get; set; }
+        public string UserFullName { get; set; }
     }
 }
